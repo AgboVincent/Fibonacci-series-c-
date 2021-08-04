e742b1d072f5de8cd5ac67385ea7ce9f8dad9dd5
z dev <agbovincent100@gmail.com>
2021-08-03 20:04:20 -0800

    Add project files.

---
+++ Fibonacci series/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Fibonacci_series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fibonacci series");
+
+            int temp = 0;
+            int number = 1;
+            for(int i = 0; i<10; i++)
+            {
+                Console.WriteLine(temp);
+                int addedNumber =number + temp;
+                temp = number;
+                number = addedNumber;
+                addedNumber = temp;
+                
+            }
+        }
+    }
+}
